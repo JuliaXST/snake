8d0e49fe31437ce1177c01f26895f1afa8c909d8
Yulia Malkova <olbanskaycat@gmail.com>
2016-02-25 00:44:48 +0300

    add class Main (explanation of RAM work)

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,21 +10,73 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
-           /* p1.x = 1;
+            Point p1 = new Point();
+            p1.x = 1;
             p1.y = 3;
-            p1.sym = '*';*/
-            p1.Draw();
+            p1.sym = '*';
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
-            p2.Draw();
-
-            Console.ReadLine();
+            Point p2 = new Point(4, 5, '#');
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine("p1=p1.x p1.x=" + p1.x + ", p1.y=" + p1.y + "; p2.x=" + p2.x + ", p2.y=" + p2.y);
+        }
+            public static void Main()
+        {
+            int x = 1;
+            Func1(x);
+            Console.WriteLine("Call Func1. x=" + x);
+        }
+        public static void Func1(int value)
+        {
+            value = value + 1;
         }
 
-       
+        public static void Main()
+        {
+            int x = 1;
+            Func2(x);
+            Console.WriteLine("Call Func2. x=" + x);
+        }
+        public static void Func2(int value)
+        {
+            value = value + 1;
+        }
+
+        public static void Main()
+        {
+            int x = 1;
+            Func3(x);
+            Console.WriteLine("Call Func3. x=" + x);
+        }
+        public static void Func3(int x)
+        {
+            x = x + 1;
+        }
+
+        public static void Main()
+        {
+            Point p1 = new Point(1, 3, '*');
+            Move(p1, 10, 10);
+            Console.WriteLine("Call Move. p1.x=" + p1.x + ", p1.y=" + p1.y);
+        }
+
+        public static void Move(Point p, int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y + dy;
+        }
+
+        public static void Main()
+        {
+            Point p2 = new Point(4, 5, '#');
+            Reset(p2);
+        }
+
+        public static void Reset(Point p)
+        {
+            p = new Point(); 
+        }
+
     }
 }
