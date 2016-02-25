7140cfafddc14b5a2a0dbf6d2c1f8226e28e002a
Yulia Malkova <olbanskaycat@gmail.com>
2016-02-25 22:25:31 +0300

    Add new class HorizontalLine

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -8,79 +8,19 @@ namespace Snake
 {
     class Program
     {
-        public static void main (string[] args)
-    
-        {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
-
-            Point p2 = new Point(4, 5, '#');
-            p1 = p2;
-            p2.x = 8;
-            p2.y = 8;
-            Console.WriteLine("p1=p1.x p1.x=" + p1.x + ", p1.y=" + p1.y + "; p2.x=" + p2.x + ", p2.y=" + p2.y);
-        }
-            public static void main()
-        {
-            int x = 1;
-            Func1(x);
-            Console.WriteLine("Call Func1. x=" + x);
-        }
-        public static void Func1(int value)
-        {
-            value = value + 1;
-        }
-
-
-        public static void main()
-        {
-            int x = 1;
-            Func2(x);
-            Console.WriteLine("Call Func2. x=" + x);
-        }
-        public static void Func2(int value)
-        {
-            value = value + 1;
-        }
-
-        public static void main()
-        {
-            int x = 1;
-            Func3(x);
-            Console.WriteLine("Call Func3. x=" + x);
-        }
-        public static void Func3(int x)
-        {
-            x = x + 1;
-        }
-
-
-        public static void main()
+        public static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            Move(p1, 10, 10);
-            Console.WriteLine("Call Move. p1.x=" + p1.x + ", p1.y=" + p1.y);
-        }
+            p1.Draw();
 
-
-        public static void Move(Point p, int dx, int dy)
-        {
-            p.x = p.x + dx;
-            p.y = p.y + dy;
-        }
-
-        public static void main()
-        {
             Point p2 = new Point(4, 5, '#');
-            Reset(p2);
-        }
+            p2.Draw();
 
-        public static void Reset(Point p)
-        {
-            p = new Point(); 
-        }
+            HorizontalLine line = new HorizontalLine(5,10,8,'+');
+            line.Draw();
 
+            Console.ReadLine();
+        }
     }
+
 }
