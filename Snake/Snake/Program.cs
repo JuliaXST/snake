d54fc56e98544aca9bd6bbd5b90159197ceed96a
Yulia Malkova <olbanskaycat@gmail.com>
2016-03-03 20:25:01 +0300

    add conclusions: snake cannot eat itself+eat walls

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,27 +12,24 @@ namespace Snake
         public static void Main(string[] args)
         {
             Console.SetBufferSize(80, 25);
-
-            //Отрисовка рамки
-            HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftline = new VerticalLine(0, 0, 24, '+');
-            VerticalLine rightline = new VerticalLine(78, 0, 24, '+');
-            upline.Draw();
-            downline.Draw();
-            leftline.Draw();
-            rightline.Draw();
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
             //Отрисовка точек
             Point p = new Point(4, 5, '*');
-            Snake snake = new Snake(p,4,Direction.RIGHT);
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
             FoodCreator foodCreator = new FoodCreator(80, 25, '$');
             Point food = foodCreator.CreateFood();
             food.Draw();
+
             while (true)
             {
+                if (walls.IsHit(snake)||snake.IsHitTail())
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
